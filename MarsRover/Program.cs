1901038ccfc0686e9826f0611e190748df02a1ec
Omer Turkyilmaz <omerturkyilmaz.09@gmail.com>
2020-02-25 10:08:17 +0300

    control

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -15,16 +15,16 @@ namespace MarsRover
             Plateau plateau = new Plateau("Mars",new Coordinate(5,5));
 
             Rover rover1 = new Rover(plateau, new Coordinate(1,2), Direction.N);
-            //rover1.Start("LMLMLMLMM");
-            rover1.Start("LMM");
+            rover1.Start("LMLMLMLMM");
+           
 
-            //Rover rover2 = new Rover(plateau, new Coordinate(3, 3), Direction.E);
-            //rover2.Start("MMRMMRMRRM");
+            Rover rover2 = new Rover(plateau, new Coordinate(3, 3), Direction.E);
+            rover2.Start("MMRMMRMRRM");
 
-            Console.WriteLine("Test Input:\n5 5\n1 2 N\nLMM\n\nExpected Output :");
-           // Console.WriteLine("Test Input:\n5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM\n\nExpected Output :");
+            
+            Console.WriteLine("Test Input:\n5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM\n\nExpected Output :");
             Console.WriteLine($"{rover1.Coordinate.X} {rover1.Coordinate.Y} {rover1.Direction.ToString()}");
-            //Console.WriteLine($"{rover2.Coordinate.X} {rover2.Coordinate.Y} {rover2.Direction.ToString()}");
+            Console.WriteLine($"{rover2.Coordinate.X} {rover2.Coordinate.Y} {rover2.Direction.ToString()}");
 
 
             Console.ReadLine();
